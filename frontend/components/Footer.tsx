79a870d3951178166108e8eb38216e5fc4c7a081
Arthur Barraine <arthur@MacBook-Pro-de-Arthur.local>
2026-09-09 13:41:00 +0200

    Footer: drop 'Sepolia testnet' label

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="foot">
-      <span>© Cloud Credits · Sepolia testnet</span>
+      <span>© Cloud Credits</span>
       <div className="foot-right">
         <a href="https://github.com/abaresks24/cloud-credits" target="_blank" rel="noreferrer">GitHub</a>
         <a href="https://x.com/abaresks" target="_blank" rel="noreferrer">Twitter</a>
